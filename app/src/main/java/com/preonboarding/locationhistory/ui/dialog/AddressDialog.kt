43c4338a36b4bc2b4b41f41b0dc86cc7dcef350b
simjunbo <simuelunbo@gmail.com>
2022-09-23 13:05:14 +0900

    feat: dialog 관련 추가

---
+++ app/src/main/java/com/preonboarding/locationhistory/ui/dialog/AddressDialog.kt
@@ -0,0 +1,95 @@
+package com.preonboarding.locationhistory.ui.dialog
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Point
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+
+import com.preonboarding.locationhistory.databinding.DialogAddressBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AddressDialog: DialogFragment() {
+
+
+    private lateinit var binding: DialogAddressBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DialogAddressBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        showAddress()
+        clickEvent()
+
+        return binding.root
+    }
+
+
+    private fun showAddress() {
+
+//        mainViewModel.addressTranslation.observe(this, Observer {
+//            binding.textViewAddressTranslation.setText(it)
+//        })
+
+    }
+
+    private fun clickEvent() {
+
+        binding.apply {
+            buttonAddressSubmit.setOnClickListener{
+                dialog?.dismiss()
+            }
+
+            buttonAddressCancel.setOnClickListener{
+                dialog?.dismiss()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setUpDialogFragment()
+    }
+
+    private fun setUpDialogFragment() {
+        val windowManager = context?.getSystemService(Context.WINDOW_SERVICE) as? WindowManager
+
+        if (Build.VERSION.SDK_INT < 30) {
+            val display = windowManager?.defaultDisplay
+            val size = Point()
+
+            display?.getSize(size)
+
+            val width = (size.x * 0.8).toInt()
+            val height = WindowManager.LayoutParams.WRAP_CONTENT
+            val window = dialog?.window
+
+            window?.setLayout(width, height)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        } else {
+            val rect = windowManager?.currentWindowMetrics?.bounds
+            val window = dialog?.window
+
+            if (rect != null) {
+                val width = (rect.width() * 0.8).toInt()
+                val height = WindowManager.LayoutParams.WRAP_CONTENT
+                window?.setLayout(width,height)
+            }
+        }
+    }
+}
